c73322d6ef77787a41e1927a82e2a3ff51f17848
aaadiii14 <107793756+aaadiii14@users.noreply.github.com>
2026-03-14 23:35:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const button = document.getElementById("startButton");
+const music = document.getElementById("music");
 
+button.addEventListener("click", function(){
+
+music.play();
+
+alert("Your surprise begins now 🧶");
+
+});
