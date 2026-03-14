f9b2fdff46a0760bde073948dda51069c8f01347
aaadiii14 <107793756+aaadiii14@users.noreply.github.com>
2026-03-15 00:30:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+
 const startButton = document.getElementById("startButton");
 const home = document.getElementById("home");
 const letterPage = document.getElementById("letterPage");
 const envelope = document.getElementById("envelope");
 const letter = document.getElementById("letter");
 
-startButton.addEventListener("click", function(){
+startButton.onclick = function() {
+    home.style.display = "none";
+    letterPage.style.display = "block";
+};
 
-home.style.display = "none";
-letterPage.style.display = "block";
-
-});
-
-envelope.addEventListener("click", function(){
-
-letter.style.display = "block";
+envelope.onclick = function() {
+    letter.style.display = "block";
+};
 
 });
